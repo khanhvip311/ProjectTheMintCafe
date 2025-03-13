55b4199620b1165f6481f1d96069dc9fee4a41aa
Kenji <khanhvip311@gmail.com>
2025-03-13 07:17:21 +0700

    Order

---
+++ ManagementCafe/ManagementCafe/Controllers/StaffController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ManagementCafe.Controllers
+{
+    public class StaffController : Controller
+    {
+        public IActionResult Order()
+        {
+            return View();
+        }
+    }
+}
