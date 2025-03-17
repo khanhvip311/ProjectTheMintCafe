6fa5657bf1842a5a644a21812fc97ad3723e1047
Kenji <khanhvip311@gmail.com>
2025-03-18 04:20:37 +0700

    update booking, order

--- ManagementCafe/ManagementCafe/obj/Debug/net9.0/ManagementCafe.AssemblyInfo.cs
+++ ManagementCafe/ManagementCafe/obj/Debug/net9.0/ManagementCafe.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ManagementCafe")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1ec11e8c84d14a621977c5df851454824daa732a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+35af745bd145f0af53b6e93b67bb7d08d4827ec4")]
 [assembly: System.Reflection.AssemblyProductAttribute("ManagementCafe")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ManagementCafe")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
